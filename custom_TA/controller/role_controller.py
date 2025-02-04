3b9549905652b3c2fc80f47c870bc4a4ec014be9
dewodewo31 <dewodewo3104@gmail.com>
2025-02-04 13:23:17 +0800

    Tampilan TA

--- custom_TA/controller/role_controller.py
+++ custom_TA/controller/role_controller.py
@@ -3,12 +3,29 @@ from odoo.http import request
 
 class RoleController(http.Controller):
 
-    @http.route('/business_owner', type='http', auth='public', website=True)
-    def business_owner_page(self, **kwargs):
-        # Render the business owner template
-        return request.render('custom_TA.business_template', {})
+      @http.route('/business_owner', type='http', auth='public', website=True)
+      def business_owner_page(self, **kwargs):
+        # Ambil partner yang merupakan perusahaan dan memiliki invoice
+        partners = request.env['res.partner'].search([
+            ('is_company', '=', True),  # Hanya Business Owner (Perusahaan)
+            ('invoice_ids', '!=', False)  # Hanya yang memiliki invoice
+        ])
+        
+        # Kirim data ke template
+        return request.render('custom_TA.business_template', {
+            'partners': partners,
+        })
 
-    @http.route('/car_owner', type='http', auth='public', website=True)
-    def car_owner_page(self, **kwargs):
-        # Render the car owner template
-        return request.render('custom_TA.car_template', {})
+      @http.route('/car_owner', type='http', auth='public', website=True)
+      def car_owner_page(self, **kwargs):
+        # Ambil partner yang memiliki invoice (status posted)
+        partners = request.env['res.partner'].search([
+            ('is_company', '=', False),
+            ('invoice_ids', '!=', False)  # Pastikan partner memiliki invoice
+        ])
+        
+        # Kirim data ke template
+        return request.render('custom_TA.car_template', {
+            'partners': partners,
+        })
+
